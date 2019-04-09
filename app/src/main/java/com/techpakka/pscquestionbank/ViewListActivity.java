4d820413e103fecdaf0b89556e73122745c3912b
jobin jeeji <jobinj1048@gmail.com>
2019-04-09 17:31:01 +0530

    add questionsview2

--- app/src/main/java/com/techpakka/pscquestionbank/ViewListActivity.java
+++ app/src/main/java/com/techpakka/pscquestionbank/ViewListActivity.java
@@ -5,8 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
